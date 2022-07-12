8339561a7d0d6ec49ac9f40370404185aa16e42f
João <moreirajp12@hotmail.com>
2022-07-12 00:04:10 -0300

    Static Frontend

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,8 +1,12 @@
-import NotificationButton from "../NotificationButton"
+import NotificationButton from "../NotificationButton";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import "./style.css"
+import "./style.css";
+import { useState } from "react";
 function SalesCard() {
+
+    const [minDate, setMinDate] = useState(new Date());
+    const [maxDate, setMaxDate] = useState(new Date());
     return (
         <>
             <div className="dsmeta-card">
@@ -10,16 +14,16 @@ function SalesCard() {
                 <div>
                     <div className="dsmeta-form-control-container">
                         <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => { }}
+                            selected={minDate}
+                            onChange={(date: Date) => setMinDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
                     </div>
                     <div className="dsmeta-form-control-container">
                         <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => { }}
+                            selected={maxDate}
+                            onChange={(date: Date) => setMaxDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
@@ -89,4 +93,4 @@ function SalesCard() {
     )
 }
 
-export default SalesCard
+export default SalesCard;
