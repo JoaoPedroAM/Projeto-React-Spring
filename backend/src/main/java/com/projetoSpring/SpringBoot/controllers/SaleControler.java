8ecccc5f234d7ee56cc94b913695e85c941a209f
João <moreirajp12@hotmail.com>
2022-07-15 15:04:11 -0300

    Bug fix

--- backend/src/main/java/com/projetoSpring/SpringBoot/controllers/SaleControler.java
+++ backend/src/main/java/com/projetoSpring/SpringBoot/controllers/SaleControler.java
@@ -6,10 +6,7 @@ import com.projetoSpring.SpringBoot.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,9 +27,9 @@ public class SaleControler {
         return service.findSales(minDate, maxDate, pageable);
     }
 
-    @GetMapping("/notification")
-    public void notifySms(){
-        smsService.sendSms();
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
 
     }
 
--- backend/src/main/java/com/projetoSpring/SpringBoot/services/SmsService.java
+++ backend/src/main/java/com/projetoSpring/SpringBoot/services/SmsService.java
@@ -1,9 +1,12 @@
 package com.projetoSpring.SpringBoot.services;
 
 
+import com.projetoSpring.SpringBoot.entities.Sale;
+import com.projetoSpring.SpringBoot.repositories.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -22,14 +25,19 @@ public class SmsService {
     @Value("${twilio.phone.to}")
     private String twilioPhoneTo;
 
-    public void sendSms() {
+    @Autowired
+    private SaleRepository saleRepository;
 
+    public void sendSms(Long saleId) {
+        Sale sale = saleRepository.findById(saleId).get();
+        String date = sale.getDate().getMonthValue()+"/"+sale.getDate().getYear();
+        String msg = "Vendedor " + sale.getSellerName()+" foi destaque em "+ date + " com um total de R$ " + String.format("%.2f",sale.getAmount());
         Twilio.init(twilioSid, twilioKey);
 
         PhoneNumber to = new PhoneNumber(twilioPhoneTo);
         PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-        Message message = Message.creator(to, from, "Teste").create();
+        Message message = Message.creator(to, from, msg).create();
 
         System.out.println(message.getSid());
     }
